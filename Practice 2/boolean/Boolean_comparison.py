13b8d4e787f41f5c08540aed513f841b9041333d
ernarlop <ernarlo0p@gmail.com>
2026-02-04 21:48:45 +0500

    Added Practice 02

---
+++ Practice 2/boolean/Boolean_comparison.py
@@ -0,0 +1,30 @@
+#Comparison Operators
+
+a = 5
+b = 10
+print(a == b)
+print(a != b)
+print(a > b)
+print(a < b)
+print(a >= b)
+print(a <= b)
+print(not(a < b))
+print((a < b) and (a != b))
+print((a < b) or (a == b))
+
+a = 7
+b = 13
+print(a > b)
+
+x = 9
+y = 15
+print(x > y)
+
+
+e = 7
+u = 13
+print(e <= u)
+
+w = 12
+q= 87
+print(w <= q)
